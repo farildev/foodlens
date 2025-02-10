92e1e715c03e7617d90f251fe1d9c8a73044f49a
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-11 00:17:42 +0400

    feat : Settings page structure created

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import Fonts from "@/constants/Fonts";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import { useState } from "react";
 
 const SettingsScreen = () => {
+  const { colors } = useTheme(); // `colors` dəyərləri burada gəlir
+  const [change, setChange] = useState(false);
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={styles.container}>
+      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+        <Text style={styles.listText}>Change Theme</Text>
+        <Switch value={change} onValueChange={() => setChange(!change)} />
+      </View>
+      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+        <Text style={styles.listText}>Change Theme</Text>
+        <Switch value={change} onValueChange={() => setChange(!change)} />
+      </View>
+      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+        <Text style={styles.listText}>Change Theme</Text>
+        <Switch value={change} onValueChange={() => setChange(!change)} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SettingsScreen
+export default SettingsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  listStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+  },
+  listText: {
+    fontFamily: Fonts["500Medium"],
+    fontSize: 16,
+  },
+});
