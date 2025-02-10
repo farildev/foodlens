eadf6c7984fcdb4ace4abe3352c573535f8633ac
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-10 13:00:34 +0400

    chore : All configurations completed

--- App.jsx
+++ App.jsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react';
+import RootNavigation from '@/navigations/RootNavigation';
+import FontLoader from '@/hocs/FontLoader';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GestureHandlerRootView style={{flex : 1}}>
+      <SafeAreaProvider>
+        <FontLoader>
+          <RootNavigation />
+        </FontLoader>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
