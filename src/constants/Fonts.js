65baa1d89be13566df2e83d67cd9fb0243c686bc
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-10 13:01:31 +0400

    chore : All constant files created

---
+++ src/constants/Fonts.js
@@ -0,0 +1,13 @@
+const Fonts = {
+  '100Thin': 'DMSans_100Thin',
+  '200ExtraLight': 'DMSans_200ExtraLight',
+  '300Light': 'DMSans_300Light',
+  '400Regular': 'DMSans_400Regular',
+  '500Medium': 'DMSans_500Medium',
+  '600SemiBold': 'DMSans_600SemiBold',
+  '700Bold': 'DMSans_700Bold',
+  '800ExtraBold': 'DMSans_800ExtraBold',
+  '900Black': 'DMSans_900Black',
+};
+
+export default Fonts;
