e4d47b5c60d9577a12be296824c08ace2116b24a
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-10 13:00:45 +0400

    feat : Screens created

---
+++ src/screens/MealScanScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealScanScreen = () => {
+  return (
+    <View>
+      <Text>MealScanScreen</Text>
+    </View>
+  )
+}
+
+export default MealScanScreen
+
+const styles = StyleSheet.create({})
