f30e48d3f8d44fa8ccaf3461e5294fea29acda10
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-11 00:18:11 +0400

    feat : main color changed

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,29 +1,28 @@
 export const Colors = {
-
-  'dark-bg': '#151515',
-  'light-bg': '#fff',
-  'main-green' : "#118B50",
-  'light-dark': '#1d1d1d',
-  'light-gray': '#cecece',
-  mainColor : "#0085FF",
+  "dark-bg": "#151515",
+  "light-bg": "#fff",
+  "main-green": "#118B50",
+  "light-dark": "#1d1d1d",
+  "light-gray": "#cecece",
+  mainColor: "#118B50",
 
   light: {
-    background: '#fff',
-    cardItem: '#f5f5f7',
-    text: '#151515',
-    border: '#ededed',
-    tabBarBg : "#151515",
-    button : "#000",
-    placeholder : "#cecece"
+    background: "#fff",
+    cardItem: "#f5f5f7",
+    text: "#151515",
+    border: "#ededed",
+    tabBarBg: "#151515",
+    button: "#000",
+    placeholder: "#cecece",
   },
 
   dark: {
-    background: '#151515',
-    cardItem: '#1d1d1d',
-    text: '#fff',
-    border: '#282828',
-    tabBarBg : "#1d1d1d",
-    button : "#000",
-    placeholder : '#cdcdcd'
-  }
-}
+    background: "#151515",
+    cardItem: "#1d1d1d",
+    text: "#fff",
+    border: "#282828",
+    tabBarBg: "#1d1d1d",
+    button: "#000",
+    placeholder: "#cdcdcd",
+  },
+};
