6c7a39ef124ff1ee6d43a3b5d5ca9873a57f459c
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-11 00:26:20 +0400

    ref : Settings screen updated

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,5 +1,6 @@
 import Fonts from "@/constants/Fonts";
-import { StyleSheet, Text, View, Switch } from "react-native";
+import Feather from '@expo/vector-icons/Feather'
+import { StyleSheet, Text, View, Switch, Pressable } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { useState } from "react";
 
@@ -8,17 +9,23 @@ const SettingsScreen = () => {
   const [change, setChange] = useState(false);
   return (
     <View style={styles.container}>
-      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-        <Text style={styles.listText}>Change Theme</Text>
-        <Switch value={change} onValueChange={() => setChange(!change)} />
-      </View>
-      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-        <Text style={styles.listText}>Change Theme</Text>
-        <Switch value={change} onValueChange={() => setChange(!change)} />
-      </View>
-      <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-        <Text style={styles.listText}>Change Theme</Text>
-        <Switch value={change} onValueChange={() => setChange(!change)} />
+      <View style={styles.listContainer}>
+        <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <Text style={styles.listText}>Dark mode</Text>
+          <Switch value={change} onValueChange={() => setChange(!change)} />
+        </View>
+        <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <Text style={styles.listText}>Change language</Text>
+          <Feather name="chevron-right" size={20} />
+        </Pressable>
+        <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <Text style={styles.listText}>Privacy Policy</Text>
+          <Feather name="chevron-right" size={20} />
+        </Pressable>
+        <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <Text style={styles.listText}>FAQ</Text>
+          <Feather name="chevron-right" size={20} />
+        </Pressable>
       </View>
     </View>
   );
@@ -31,12 +38,16 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
   },
+  listContainer : {
+    marginTop : 12
+  },
   listStyle: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 12,
     borderBottomWidth: 1,
+    height: 56
   },
   listText: {
     fontFamily: Fonts["500Medium"],
