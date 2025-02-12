f3244e856dd9f0ef2453289b905fe738e30d5c63
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-12 13:49:00 +0400

    feat : Settings screen completed

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,33 +1,76 @@
 import Fonts from "@/constants/Fonts";
-import Feather from '@expo/vector-icons/Feather'
-import { StyleSheet, Text, View, Switch, Pressable } from "react-native";
+import { Feather, Ionicons, MaterialIcons } from '@expo/vector-icons'
+import { StyleSheet, Text, View, Pressable, Button } from "react-native";
 import { useTheme } from "@react-navigation/native";
-import { useState } from "react";
-
+import { Colors } from "@/constants/Colors";
+import { useCallback, useRef, useMemo } from "react";
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
 const SettingsScreen = () => {
-  const { colors } = useTheme(); // `colors` dəyərləri burada gəlir
-  const [change, setChange] = useState(false);
+  const { colors } = useTheme();
+  const bottomSheetModalRef = useRef(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current.present();
+  }, []);
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
   return (
     <View style={styles.container}>
+      <BottomSheetModal
+        snapPoints={["50%"]}
+        ref={bottomSheetModalRef}
+        onChange={handleSheetChanges}
+        style={{ backgroundColor: 'red' }}
+        index={-1}
+        enablePanDownToClose={true} 
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheetModal>
       <View style={styles.listContainer}>
-        <View style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-          <Text style={styles.listText}>Dark mode</Text>
-          <Switch value={change} onValueChange={() => setChange(!change)} />
-        </View>
-        <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-          <Text style={styles.listText}>Change language</Text>
+        <Pressable onPress={handlePresentModalPress} style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <View style={styles.listInnerContainer}>
+            <View style={[styles.iconContainer, { backgroundColor: Colors["main-green"] }]}>
+              <Feather name="moon" size={18} color="#fff" />
+            </View>
+            <Text style={styles.listText}>Theme mode</Text>
+          </View>
           <Feather name="chevron-right" size={20} />
         </Pressable>
         <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-          <Text style={styles.listText}>Privacy Policy</Text>
+          <View style={styles.listInnerContainer}>
+            <View style={[styles.iconContainer, { backgroundColor: "#c50000" }]}>
+              <Ionicons name="language-outline" size={18} color="#fff" />
+            </View>
+            <Text style={styles.listText}>Change Language</Text>
+          </View>
           <Feather name="chevron-right" size={20} />
         </Pressable>
         <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
-          <Text style={styles.listText}>FAQ</Text>
+          <View style={styles.listInnerContainer}>
+            <View style={[styles.iconContainer, { backgroundColor: "#ff7700" }]}>
+              <MaterialIcons name="security" size={18} color="#fff" />
+            </View>
+            <Text style={styles.listText}>Privacy Policy</Text>
+          </View>
+          <Feather name="chevron-right" size={20} />
+        </Pressable>
+        <Pressable style={[styles.listStyle, { borderBottomColor: colors.border }]}>
+          <View style={styles.listInnerContainer}>
+            <View style={[styles.iconContainer, { backgroundColor: "#0088FF" }]}>
+              <MaterialIcons name="question-mark" size={18} color="#fff" />
+            </View>
+            <Text style={styles.listText}>F.A.Q</Text>
+          </View>
           <Feather name="chevron-right" size={20} />
         </Pressable>
       </View>
-    </View>
+      <View style={styles.version}>
+        <Text style={styles.versionText}>Version 1.0.0</Text>
+      </View>
+    </View >
   );
 };
 
@@ -36,21 +79,48 @@ export default SettingsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
+    paddingHorizontal: 12,
   },
-  listContainer : {
-    marginTop : 12
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'red'
+  },
+  listContainer: {
+    flex: 1,
+    marginTop: 12,
+    flexDirection: 'column',
+    gap: 8
   },
   listStyle: {
+    height: 56,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingVertical: 12,
-    borderBottomWidth: 1,
-    height: 56
+    backgroundColor: "#f7f7f7",
+    paddingHorizontal: 8,
+    borderRadius: 12,
+  },
+  listInnerContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center'
   },
   listText: {
     fontFamily: Fonts["500Medium"],
     fontSize: 16,
   },
+  iconContainer: {
+    padding: 12,
+    borderRadius: 8
+  },
+  version: {
+    paddingVertical: 16
+  },
+  versionText: {
+    fontSize: 14,
+    color: "#666666",
+    textAlign: 'center',
+    fontFamily: Fonts["500Medium"]
+  }
 });
