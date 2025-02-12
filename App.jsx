240e566b3d5045e04ba6070490a6bdf8f2c093cf
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-13 00:44:11 +0400

    feat : Provider and persist gate implemented

--- App.jsx
+++ App.jsx
@@ -4,17 +4,24 @@ import FontLoader from '@/hocs/FontLoader';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@/store/store';
+import { Provider } from 'react-redux';
 const App = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <SafeAreaProvider>
-          <FontLoader>
-            <RootNavigation />
-          </FontLoader>
-        </SafeAreaProvider>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <BottomSheetModalProvider>
+            <SafeAreaProvider>
+              <FontLoader>
+                <RootNavigation />
+              </FontLoader>
+            </SafeAreaProvider>
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
+      </PersistGate>
+    </Provider>
   )
 }
 
