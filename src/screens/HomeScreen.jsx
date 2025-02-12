6480ecc39d2b7d7d714926cef1014d9fae462f47
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-12 13:50:41 +0400

    feat : Date timeline added

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
+import { useState } from 'react';
+import { Colors } from '@/constants/Colors';
+import Fonts from '@/constants/Fonts';
 const HomeScreen = () => {
+  const daysOfWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  const [selectedDay, setSelectedDay] = useState(null);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <FlatList
+      data={daysOfWeek}
+      horizontal
+      style={{height : "100px",flex: '0', paddingHorizontal : 12}}
+      showsHorizontalScrollIndicator={false}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+        activeOpacity={0.8}
+          style={{
+            backgroundColor: selectedDay === item ? Colors['main-green'] : "#C4F3DB",
+            marginHorizontal: 8,
+            borderRadius: '50%',
+            justifyContent : 'center',
+            alignItems : 'center',
+            width : 54,
+            height : 54
+          }}
+          onPress={() => setSelectedDay(item)}
+        >
+          <Text style={{ color: selectedDay ? "white" : "#000", fontSize : 16, fontFamily : Fonts['500Medium'] }}>{item}</Text>
+        </TouchableOpacity>
+      )}
+      keyExtractor={(item) => item}
+    />
   )
 }
 
