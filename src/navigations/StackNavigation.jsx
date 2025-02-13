16501b055ffdcad6d491cd0c58407aab8d4dcea8
Faril Mammadov <memmedovfaril@gmail.com>
2025-02-13 15:57:54 +0400

    chore:  camera screen added to stack

--- src/navigations/StackNavigation.jsx
+++ src/navigations/StackNavigation.jsx
@@ -1,10 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigation from "./TabNavigation";
+import CameraScreen from "@/screens/CameraScreen";
 const StackNavigation = () => {
   const { Navigator, Screen } = createNativeStackNavigator();
   return (
     <Navigator>
       <Screen name="tabs" options={{headerShown : false}} component={TabNavigation} />
+      <Screen name="camera" options={{headerShown : false}} component={CameraScreen} />
     </Navigator>
   )
 }
